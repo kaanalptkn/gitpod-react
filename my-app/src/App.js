620aa62b0632f66169fcd4bfb74caad492223424
Kaan Alptakin <kaanalptkn@icloud.com>
2022-09-02 14:31:31 +0000

    Updated

---
+++ my-app/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import StatefulGreeting from './components/StatefulGreeting';
+
+function App() {
+  return (
+    <div className="App">
+        <StatefulGreeting greeting="I'am a stateful class component!" />
+    </div>
+  );
+}
+
+export default App;
